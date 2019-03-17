5db8ce22b5276fadecaadbe98563429d73324b42
Briana Cooke <br.cooke12@gmail.com>
2019-03-17 04:26:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,2 +1,7 @@
-def position_taken? (board="1","2","3","4","5","6","7","8","9", index= "X","O")
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
 end
